f446ead5a9ba65764ae52e17b69c38b724739f47
KUNAL SAHU <53874235+mrkunalsahu@users.noreply.github.com>
2019-11-22 14:18:53 +0530

    Add files via upload

---
+++ VID11_Vectors.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int main()
+{
+	int t=10,k=0;
+	vector <int> arr;
+/*cout<<"Enter the numbers (0 to quit) : ";
+while(cin>>t&&t){
+	arr.push_back(t);
+}
+
+cout<<"\nAll the non_zero elements entered by you are : ";
+ t=arr.size();
+// cout<<arr.size();
+while(cout<<arr[k++]<<" "&&(!(k==t)))
+{}
+
+
+*/
+arr.push_back(1);
+arr.push_back(1);
+arr.push_back(1);
+arr.push_back(1);
+arr.push_back(1);
+arr.push_back(1);
+arr.push_back(1);
+arr.push_back(1);
+cout<<arr.size();
+
+	return 0;
+}
